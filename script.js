113723b26a70fc2616e343cfe91a4779bdc5c170
sgardner01 <s.gardner01@outlook.com>
2022-02-23 01:37:59 -0700

    pikachu meme face

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 const trueButton = document.getElementById('true-btn')
 const falseButton = document.getElementById('false-btn')
 const questionBoxElement = document.getElementById('questions-box')
-const questionElement = document.getElementById('question')
-
+// const questionElement = getElementById('question')
+let shuffledQuestions, currentQuestionIndex
 
 document.getElementById("questions-box").innerHTML = "New text!"
 
-let currentQuestionIndex
+
 
 trueButton.addEventListener('click' , submitAnswer)
 falseButton.addEventListener('click', submitAnswer)
 
-//Code runs when sub button is clicked
+//Code runs when T/F buttons clicked
 function submitAnswer() {
   console.log('submitted')
-  // for (let i = 0; i < questionList.length; i++) {
-  //   console.log(questionList[i]);
   }
 
-// function setNextQuestion() {
-// }
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-// //showing of questions
-// function selectAnswer() {
-//   showQuestion(currentQuestionIndex)
-// }
+function showQuestion(question) {
+}
+//showing of questions
 
-// //Move to next quuestion
-// function showQuestion(questionsList) {
-// }
 
-let questionList = [
-  "question1", 
-  "question2", 
-  "question3",
-  "question4",
-  "question5"
-]
+
+let questionList = {question: "Question0", question: "Question1", question: "Question2"}
+// for (let i = 0; i < questionList.length; i++ )
+// console.log(questionList[0])
+
+//TO DO
+
+//Show questions
+//Increment questions
+//Button functions
