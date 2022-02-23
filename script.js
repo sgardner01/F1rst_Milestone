bb9a1abe31b2d15cd34a2588f12aaba013152337
sgardner01 <s.gardner01@outlook.com>
2022-02-22 21:15:16 -0700

    question text changed

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-const submitButton = document.getElementById('submit-btn')
+const trueButton = document.getElementById('true-btn')
+const falseButton = document.getElementById('false-btn')
 const questionBoxElement = document.getElementById('questions-box')
 const questionElement = document.getElementById('question')
 
+
+document.getElementById("questions-box").innerHTML = "New text!"
+
 let currentQuestionIndex
 
-submitButton.addEventListener('click' , submitAnswer)
-// trueButton.addEventListener('click', checkAnswer)
-// falseButton.addEvenetListener('click', checkAnswer)
+trueButton.addEventListener('click' , submitAnswer)
+falseButton.addEventListener('click', submitAnswer)
 
 //Code runs when sub button is clicked
 function submitAnswer() {
   console.log('submitted')
-  for (let i = 0; i < questionList.length; i++) {
-    console.log(questionList[i]);
+  // for (let i = 0; i < questionList.length; i++) {
+  //   console.log(questionList[i]);
   }
-  setNextQuestion()
-}
 
-function setNextQuestion() {
-}
+// function setNextQuestion() {
+// }
 
-//showing of questions
-function selectAnswer() {
-  showQuestion(currentQuestionIndex)
-}
+// //showing of questions
+// function selectAnswer() {
+//   showQuestion(currentQuestionIndex)
+// }
 
-//Move to next quuestion
-function showQuestion(questionsList) {
-}
+// //Move to next quuestion
+// function showQuestion(questionsList) {
+// }
 
 let questionList = [
-  'question1', 
-  'question2', 
-  'question3',
-  'question4',
-  'question5'
+  "question1", 
+  "question2", 
+  "question3",
+  "question4",
+  "question5"
 ]
